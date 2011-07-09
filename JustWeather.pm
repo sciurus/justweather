4063ff1fe64354980c213b70ffb8af3a36b136de
Brian Pitts <brian@polibyte.com>
2011-07-09 19:41:28 -0400

    Add proper error handling

--- JustWeather.pm
+++ JustWeather.pm
@@ -12,8 +12,9 @@ use XML::LibXML;
 use XML::LibXSLT;
 
 sub new {
+    my $class = shift;
     my $self = {};
-    bless $self;
+    bless $self, $class;
     return $self;
 }
 
@@ -21,18 +22,17 @@ sub run {
     my $self = shift;
     my $env = shift;
 
-    my $req = Plack::Request->new($env);
-    print "Processing request\n\n\n";
-    my $zip = $req->param('zip');
+    $self->{req} = Plack::Request->new($env);
+    my $zip = $self->{req}->param('zip');
 
-    die "Invalid zip code $zip" unless $zip =~ /\d{5}/;
+    return $self->error('Invalid zip code') unless $zip =~ /\d{5}/;
 
     my $url   = "http://weather.yahooapis.com/forecastrss?p=$zip";
     my $first = substr($zip, 0, 1);
     my $path  = "cache/$first/$zip.html";
 
     my $weather_rss = get($url);
-    die "Couldn't get $url" unless defined $weather_rss;
+    return $self->error('Could not load weather data') unless defined $weather_rss;
 
     my $parser     = XML::LibXML->new();
     my $xslt       = XML::LibXSLT->new();
@@ -42,11 +42,25 @@ sub run {
     my $xml        = $stylesheet->transform($source);
     my $html       = $stylesheet->output_as_chars($xml); 
 
-    open my $fh, '>', $path or die "Could not write cache $path";
+    open my $fh, '>', $path or return $self->error('Could not write to cache');
     print $fh $html;
     close $fh;
 
-    my $res = $req->new_response(200);
+    my $res = $self->{req}->new_response(200);
+    $res->content_type('text/html');
+    $res->body($html);
+
+    return $res->finalize;
+}
+
+sub error {
+    my $self = shift;
+    my $description = shift;
+
+    my $html = "<html><head><title>Error</title></head>" .
+    "<body>Error: $description</body></html>";
+
+    my $res = $self->{req}->new_response(500);
     $res->content_type('text/html');
     $res->body($html);
 
