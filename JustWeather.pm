4574f23a982ab266fc7374fa4737cc71afbac852
Brian Pitts <brian@polibyte.com>
2011-07-10 23:41:42 -0400

    Use request's uri rather than query string to get zip code. Rename cache folder to make nginx config simpler. Add nginx config.

--- JustWeather.pm
+++ JustWeather.pm
@@ -13,25 +13,32 @@ use XML::LibXSLT;
 
 sub new {
     my $class = shift;
-    my $self = {};
+    my $self  = {};
     bless $self, $class;
     return $self;
 }
 
 sub run {
     my $self = shift;
-    my $env = shift;
+    my $env  = shift;
 
     $self->{req} = Plack::Request->new($env);
-    my $zip = $self->{req}->param('zip');
 
-    return $self->error('Invalid zip code') unless $zip =~ /\d{5}/;
+    my $zip;
+
+    # match path /zip/12345 or /zip/12345/ but no more than 5 digits
+    if ( $self->{req}->path_info =~ /\/zip\/(\d{5})\/?$/ ) {
+        $zip = $1;
+    }
+    else {
+        return $self->error('Invalid zip code');
+    }
 
     my $url   = "http://weather.yahooapis.com/forecastrss?p=$zip";
-    my $path  = "cache/$zip.html";
+    my $cache = "zip/$zip.html";
 
     my $weather_rss = get($url);
-    return $self->error('Could not load weather data') unless defined $weather_rss;
+    return $self->error('Could not load weather') unless defined $weather_rss;
 
     my $parser     = XML::LibXML->new();
     my $xslt       = XML::LibXSLT->new();
@@ -39,9 +46,9 @@ sub run {
     my $style_doc  = $parser->parse_file('justweather.xsl');
     my $stylesheet = $xslt->parse_stylesheet($style_doc);
     my $xml        = $stylesheet->transform($source);
-    my $html       = $stylesheet->output_as_chars($xml); 
+    my $html       = $stylesheet->output_as_chars($xml);
 
-    open my $fh, '>', $path or return $self->error('Could not write to cache');
+    open my $fh, '>', $cache or return $self->error('Could not write to cache');
     print $fh $html;
     close $fh;
 
@@ -53,11 +60,11 @@ sub run {
 }
 
 sub error {
-    my $self = shift;
+    my $self        = shift;
     my $description = shift;
 
-    my $html = "<html><head><title>Error</title></head>" .
-    "<body>Error: $description</body></html>";
+    my $html = "<html><head><title>Error</title></head>"
+      . "<body>Error: $description</body></html>";
 
     my $res = $self->{req}->new_response(500);
     $res->content_type('text/html');
